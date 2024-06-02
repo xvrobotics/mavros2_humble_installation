9b191fe12082e105ac1f9ffa50e1e81467e7e23f
Xavier Vicent Navarro <134575240+xvrobotics@users.noreply.github.com>
2024-06-02 10:09:12 -0400

    Update mavros2_install.sh

--- mavros2_install.sh
+++ mavros2_install.sh
@@ -6,20 +6,32 @@ set -e
 # Update package lists
 sudo apt update
 
+sudo apt upgrade
+
+ROS2_WS=~/mavros2_ws
+
+# Delete existing install, log, and build directories if they exist
+if [ -d "$ROS2_WS/install" ]; then
+  rm -rf $ROS2_WS/install
+fi
+
+if [ -d "$ROS2_WS/log" ]; then
+  rm -rf $ROS2_WS/log
+fi
+
+if [ -d "$ROS2_WS/build" ]; then
+  rm -rf $ROS2_WS/build
+fi
+
 # Install necessary dependencies
 sudo apt install -y python3-vcstool python3-rosinstall-generator python3-osrf-pycommon python3-colcon-common-extensions python3-rosdep git wget
 
-# Create the workspace
-mkdir -p ~/mavros2_ws/src
+source /opt/ros/humble/setup.bash
+
 cd ~/mavros2_ws
 
-# Install dependencies
-sudo rosdep init || echo "rosdep already initialized"
-rosdep update
-rosdep install --from-paths src --ignore-src -y
-
 # Install GeographicLib datasets
-./src/mavros/mavros/scripts/install_geographiclib_datasets.sh
+sudo ./src/mavros/mavros/scripts/install_geographiclib_datasets.sh
 
 # Build the workspace
 colcon build
@@ -28,5 +40,6 @@ colcon build
 echo "source ~/mavros2_ws/install/setup.bash" >> ~/.bashrc
 source ~/mavros2_ws/install/setup.bash
 
-echo "MAVROS installation for ROS 2 Foxy completed successfully."
+echo "MAVROS installation for ROS 2 completed successfully."
+
 
